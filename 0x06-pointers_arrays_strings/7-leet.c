5229ddf8475c2a2a59bdf0793cecbdef6c5ce191
GodmanOluwaseun <oluwaseuntopeajayi@gmail.com>
2023-08-28 11:38:51 -0700

    Function that encodes a string to leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *leet- Encodes a string into 1337
+ *
+ * @s: string to be encoded
+ * Return: pointer to encoded string.
+ */
+
+char *leet(char *s)
+{
+	int a = 0;
+	int b = 0;
+	char leet[] = "aeotlAEOTL";
+	char leetnum[] = "4307143017";
+
+	while (s[a] != '\0')
+	{
+		b = 0;
+		for ( ; leet[b] != '\0'; b++)
+		{
+			if (s[a] == leet[b])
+			{
+				s[a] = leetnum[b];
+				break;
+			}
+		}
+		a++;
+	}
+	return (s);
+}
