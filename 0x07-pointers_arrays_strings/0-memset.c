d1b293f37ec74a690345b974a924dab1b43ac0a4
GodmanOluwaseun <oluwaseuntopeajayi@gmail.com>
2023-08-28 20:13:46 -0700

    Function that copies memory area.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset- Function fills the first n bytes of the memory area pointed
+ * to by s with the constant byte b.
+ *
+ * @s: Pointer to mem location.
+ * @b: byte to  fe filled in.
+ * @n: numbe of bytes to fill.
+ * Return: pointer to modified bytes.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+
+}
