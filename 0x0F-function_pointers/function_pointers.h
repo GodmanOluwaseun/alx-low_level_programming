f011b749c3abb9537f45478c57cc9beff5864d17
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2023-11-09 18:31:54 -0800

    Function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,4 +15,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif /* MAIN_H */
+#endif /* FUNCTION_POINTERS_H */
