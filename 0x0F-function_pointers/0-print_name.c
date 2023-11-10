52896049914919a5f3cae39679a1614274894ebb
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2023-11-09 19:34:35 -0800

    Function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -12,22 +12,3 @@ void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }
-
-/**
- * print - Print string content.
- *
- * @s: Pointer to string
- * Return: void.
- */
-
-void print(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-	_putchar(s[i]);
-	i++;
-	}
-	_putchar('\n');
-}
