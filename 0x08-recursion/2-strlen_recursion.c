ddec0b3efa3823f8b39fa744a12aa78bb047c5d7
GodmanOluwaseun <oluwaseuntopeajayi@gmail.com>
2024-01-11 04:31:13 -0800

    Function that prints a string in reverse.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Function that returns the length of a string.
+ *
+ * @s: Pointer to string
+ * Return: Length of string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	
+}
