12222c4cdc6640930d4ff012494060c7726df3ae
GodmanOluwaseun <oluwaseuntopeajayi@gmail.com>
2024-01-13 11:48:59 -0800

    Pogram that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Pogram that prints the number of arguments passed into it.
+ *
+ * @argc: No of arguments.
+ * @argv: Argument vectors.
+ * Return: (0).
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv
+
+	if (argc >= 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
