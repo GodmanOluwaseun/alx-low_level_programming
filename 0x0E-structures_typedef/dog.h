82c1fa27972c3d6348e7b620997be0497b7c6d33
GodmanOluwaseun <oluwaseuntopeajayi@gmail.com>
2024-01-25 10:16:53 -0800

    #include <stdio.h>

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - A new struct with 3 members.
+ *
+ * @name: Pointer to name string.
+ * @age: Float containing age.
+ * @owner: Pointer to dog owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
