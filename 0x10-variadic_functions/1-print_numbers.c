75ff8c7992c0ebc881c59b0cc661cab4a1ad79e2
GodmanOluwaseun <oluwaseuntopeajayi@gmail.com>
2024-01-27 04:20:11 -0800

    Function that prints a name.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,12 +13,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (seprator == NULL)
+	if (separator == NULL)
 	{
 		return;
 	}
 
-	va_starts(args, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
