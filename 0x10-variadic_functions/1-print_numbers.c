ea76b557b995bd513edefb545bc3fa90a2d98a2e
GodmanOluwaseun <oluwaseuntopeajayi@gmail.com>
2024-01-27 04:17:01 -0800

    Function that prints a name.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Function that prints numbers, followed by a new line.
+ * @separator: Pointer to delimiter character.
+ * @n: Number of passed integers.
+ * @...: VAriable no of arguments.
+ * Return: Void.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (seprator == NULL)
+	{
+		return;
+	}
+
+	va_starts(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end(args);
+}
