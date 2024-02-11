7bac4ae513c3fd22c833e5892fd3f531f71a74ad
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-02-11 08:36:41 -0800

    Function that converts a binary number to an unsigned int.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Function that converts a binary number to an unsigned int.
+ * @b: Pointer to binary string.
+ * Return: Decimal value or 0.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec_value = 0, weight, place_value;
+
+	if (*b == NULL)
+	{
+		return (0);
+	}
+
+	place_value = strlen(b) - 1;
+
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+
+		weight = 1 << place_value;
+		dec_value += (*b - '0') * weight;
+		place_value--;
+		b++;
+	}
+
+	return (dec_value);
+}
