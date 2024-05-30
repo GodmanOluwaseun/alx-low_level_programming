29c673d39dd676b168f29b240d0296fd766eea42
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-05-30 09:57:03 -0700

    Function that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""island_perimeter
+    Returns perimeter of island described in grid argument.
+"""
+
+
+def island_perimeter(grid):
+    """Returns perimeter of island in grid"""
+
+    if not grid or not grid[0]:
+        return 0
+
+    row = len(grid)
+    col = len(grid[0])
+    counter = 0
+
+    for i in range(row):
+        for j in range(col):
+
+            if grid[i][j] == 1:
+                
+                counter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    counter -= 2
+                if j> 0 and grid[i][j - 1] == 1:
+                    counter -= 2
+    
+    return counter
