5a1d0125db7946d6053c46e8a1e13a5695079833
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-07-18 11:15:16 -0700

    linear search function

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,13 +17,11 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+
 		if (array[i] == value)
 		{
 			return (i);
-		}
-		else
-		{
-			printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
 		}
 	}
 
