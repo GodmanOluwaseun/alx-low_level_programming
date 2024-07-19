0f0df491041b7485144be5cda2d9f471bafc6cb9
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-07-19 06:40:53 -0700

    Function that searches using binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for value in sorted array of ints  using Binary search
+ * binary_search - Searches for value in sorted array using Binary search
  * @array: Pointer to array.
  * @size: Size of array.
  * @value: Value to search for.
